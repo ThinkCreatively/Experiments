48fe590be7b676b2ecef81a38a5463576031916f
James Moore <james3.moore@dish.com>
2023-11-10 14:44:52 -0700

    ~ fixes

--- experiments/experiments/src/app/page.tsx
+++ experiments/experiments/src/app/page.tsx
@@ -1,20 +1,15 @@
 "use client";
 import * as React from "react";
 import { NextUIProvider } from "@nextui-org/react";
-import { Cinzel } from "next/font/google";
 import styles from "./page.module.css";
 import TitleBox from "../components/titleBox/titleBox";
-
-const cinzel = Cinzel({ subsets: ["latin"] });
 
 const Home = () => {
   return (
     <NextUIProvider>
       <main className={styles.main}>
         <div className={styles.titleContainer}>
-          <div className={cinzel.className} style={{ fontSize: "5rem" }}>
-            Playground
-          </div>
+          <div className={styles.titleText}>Playground</div>
         </div>
         <TitleBox />
       </main>
--- experiments/experiments/src/components/titleBox/titleBox.tsx
+++ experiments/experiments/src/components/titleBox/titleBox.tsx
@@ -1,16 +1,12 @@
-import { Button } from "@nextui-org/react";
-import { Inter } from "next/font/google";
 import styles from "./titleBox.module.css";
 import React from "react";
-
-const cinzel = Inter({ subsets: ["latin"] });
 
 const TitleBox = () => {
   return (
     <div className={styles.contentContainer}>
       <div className={styles.innerContainer}>
-        <div>Content</div>
-        <div>Go</div>
+        <div>Name</div>
+        <div>Button Text</div>
       </div>
     </div>
   );
